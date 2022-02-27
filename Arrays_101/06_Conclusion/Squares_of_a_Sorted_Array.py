d620a052bdc974022e3df10aee7d64232ec8f9c2
sunandbean <sunbin.vincent.kim@gmail.com>
2022-02-27 21:10:57 +0900

    add Squares_of_a_Sorted_Array

---
+++ Arrays_101/06_Conclusion/Squares_of_a_Sorted_Array.py
@@ -0,0 +1,9 @@
+# Same with 01_Introduction/Squares_of_a_Sorted_Array.py
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        square_list = []
+        for idx, num in enumerate(nums):
+            square_list.append(num**2)
+        return sorted(square_list)
+        
